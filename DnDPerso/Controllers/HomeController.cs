c41e991b05985d6fafb8e935afb53b5c54dba735
Stéphane BINTEIN <sebintein@osiarouen.onmicrosoft.com>
2017-01-17 13:49:47 +0100

    connec bdd

--- DnDPerso/Controllers/HomeController.cs
+++ DnDPerso/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DnDPersoBLL;
+using DnDPersoEntities;
 
 namespace DnDPerso.Controllers
 {
@@ -10,6 +12,8 @@ namespace DnDPerso.Controllers
     {
         public ActionResult Index()
         {
+            List<Classe> listClasses = ClasseBLL.GetRList();
+            ViewData["listClasses"] = SetDropDownValues(listClasses, "Id", "Libelle", false);
             return View();
         }
 
@@ -23,8 +27,36 @@ namespace DnDPerso.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
+            
             return View();
+        }
+
+        public static List<SelectListItem> SetDropDownValues<T>(IList<T> objects, string idProperty, string libelleProperty, bool hasEmptyElement)
+        {
+            List<SelectListItem> listTypesEntrants = new List<SelectListItem>();
+
+            if (hasEmptyElement)
+            {
+                listTypesEntrants.Add(new SelectListItem
+                {
+                    Selected = true,
+                    Text = string.Empty,
+                    Value = "0"
+                });
+            }
+
+            foreach (var t in objects)
+            {
+                string id = t.GetType().GetProperty(idProperty).GetValue(t, null).ToString();
+                listTypesEntrants.Add(new SelectListItem
+                {
+                    Selected = false,
+                    Text = t.GetType().GetProperty(libelleProperty).GetValue(t, null).ToString(),
+                    Value = id
+                });
+            }
+
+            return listTypesEntrants;
         }
     }
 }
---
+++ DnDPersoBLL/ClasseBLL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DnDPersoBLL.Base;
+using DnDPersoEntities;
+
+namespace DnDPersoBLL
+{
+    public class ClasseBLL : BaseBLL<Classe>
+    {
+    }
+}
