396fc325ee9b2f623d5fca6babb57d4b4e3b47e3
Camille Lucer <yirmocoder@gmail.com>
2017-02-24 02:00:32 +0100

    design

--- DnDPerso/Scripts/Views/Pouvoir/Pouvoir.js
+++ DnDPerso/Scripts/Views/Pouvoir/Pouvoir.js
@@ -28,8 +28,8 @@ function searchPouvoir(classe, level) {
         $("#containerPouvoir").empty();
         $("#containerPouvoir").append(htmlDecode(result));
 
-        $(".infobox-power-title").css('cursor', 'pointer');
-        $(".infobox-power-title").on('click',
+        $(".infobox-power").css('cursor', 'pointer');
+        $(".infobox-power").on('click',
        function () {
 
            if ($.inArray(this.innerHTML, pouvoirs) === -1) {
