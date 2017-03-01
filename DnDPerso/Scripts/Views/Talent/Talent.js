2f0a4a03ce0617e3f1e878bc9f1d199f556a58cc
Camille Lucer <yirmocoder@gmail.com>
2017-03-01 13:17:15 +0100

    zadaz

--- DnDPerso/Scripts/Views/Talent/Talent.js
+++ DnDPerso/Scripts/Views/Talent/Talent.js
@@ -1,16 +1,16 @@
 ﻿var talents = [];
 
 $(document).ready(function () {
-    $(".infobox-power-title").css('cursor', 'pointer');
-    $(".infobox-power-title").on('click',
+    $(".infobox-power").css('cursor', 'pointer');
+    $(".infobox-power").on('click',
     function () {
 
         if ($.inArray(this.innerHTML, talents) === -1) {
-            talents.push(this.innerHTML);
+            talents.push($(this).find('.infobox-power-title')[0].innerHTML);
         }
 
         if ($(this).attr('class').indexOf('highLight') !== -1) {
-            talents.splice($.inArray(this.innerHTML, talents), 1);
+            talents.splice($.inArray($(this).find('.infobox-power-title')[0].innerHTML, talents), 1);
         }
         $(this).toggleClass('highLight');
 
